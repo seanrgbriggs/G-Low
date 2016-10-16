3ac26ab365738539c028be505bcd616f09a3f62e
seanrgbriggs <seanrgbriggs@gmail.com>
2016-10-16 18:38:47 -0400

    c

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+	public Transform tr;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.forward = tr.position - transform.position;
+	}
+}
