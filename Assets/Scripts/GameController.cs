bc7c1bd9ba13cf7a2f59f2e22604cba41a540227
seanrgbriggs <seanrgbriggs@gmail.com>
2016-10-17 17:04:46 -0400

    Scripts and Testing, also inputs

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,17 +4,19 @@ using System.Collections.Generic;
 
 public class GameController : MonoBehaviour {
 
-	PlayerCar[] players;
-	Queue<float> player_dists;
+	List<PlayerCar> players;
 
 	WaypointScript[] waypoints;
+
+	public int laps_max;
 
 	// Use this for initialization
 	void Start () {
 		GameObject[] player_objs = GameObject.FindGameObjectsWithTag ("Player");
-		players = new PlayerCar[player_objs.Length];
-		for (int i = 0; i < players.Length; i++) {
-			players [i] = player_objs [i].GetComponent<PlayerCar> ();
+ 
+		players = new List<PlayerCar> ();
+		for (int i = 0; i < player_objs.Length; i++) {
+			players.Add(player_objs [i].GetComponent<PlayerCar> ());
 		}
 
 
@@ -24,13 +26,16 @@ public class GameController : MonoBehaviour {
 			waypoints [i] = waypoint_objs [i].GetComponent<WaypointScript> ();
 		}
 
-		player_dists = new Queue<float> ();
-
+ 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		players.Sort (((x, y) => x.getDistance ().CompareTo (y.getDistance ())));
+		for (int i = 0; i < players.Count; i++) {
+			print (i + ": " + players.ToArray () [i].gameObject.name);
+		}
+ 	
 	}
 
 	public WaypointScript[] getWaypoints(){
--- Assets/Scripts/PlayerCar.cs
+++ Assets/Scripts/PlayerCar.cs
@@ -3,13 +3,23 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class PlayerCar : MonoBehaviour {
-    private Rigidbody rb;
+    
+
+	private Rigidbody rb;
     public Transform[] wheels;
+	public int id;
+
 
 	float distance;
-
 	int num_laps;
 	bool primedForLap;
+
+	private Vector3 last_safe_pos;
+	private const float off_time = 1.5f; //time to return when off-stage;
+	private float cur_off_time = 0.0f;
+
+	float ab_cooldown;
+	float ult_cooldown;
 
 	GameController gc;
 	List<WaypointScript> waypoints;
@@ -36,20 +46,39 @@ public class PlayerCar : MonoBehaviour {
 
 		HandleRaycast ();
 		HandleLapping ();
-		GetComponentInChildren<Camera>().transform.RotateAround(transform.position, transform.up, Input.GetAxis("Mouse X") * Time.deltaTime * 60);
+ 		//GetComponentInChildren<Camera>().transform.RotateAround(transform.position, transform.up, Input.GetAxis("Mouse X") * Time.deltaTime * 60);
     }
 
 	void HandleRaycast(){
 		RaycastHit hit;
+		bool safe = false;
 		foreach (Transform wheel in wheels) {
 			if (Physics.Raycast(wheel.position, -transform.up, out hit, 20.0f)) {
 
 				rb.AddForceAtPosition(hit.normal * 9.8f * (2.0f - hit.distance) * 0.25f, wheel.position, ForceMode.Acceleration);
 
 
-				rb.AddForce(transform.forward * Input.GetAxis("Vertical") * 5, ForceMode.Acceleration);
-				rb.AddTorque(transform.up * Input.GetAxis("Horizontal") * 1, ForceMode.Acceleration);
-							}
+				rb.AddForce(transform.forward * Input.GetAxis("Vertical"+id) * 5, ForceMode.Acceleration);
+				rb.AddTorque(transform.up * Input.GetAxis("Horizontal"+id) * 1, ForceMode.Acceleration);
+				last_safe_pos = transform.position;
+				if (!safe)
+					safe = true;
+			}
+		}
+		if (!safe) {
+			print (name + "is unsafe");
+			if (cur_off_time < off_time) {
+				cur_off_time += Time.deltaTime;
+			}else{
+				print (name + "is too slow");
+				rb.velocity = -rb.velocity;
+				transform.position = last_safe_pos + rb.velocity.normalized;
+				GameObject track = GameObject.FindGameObjectWithTag ("Track");
+				print (track.name);
+				transform.up = transform.position - track.GetComponent<Collider> ().ClosestPointOnBounds (transform.position);
+
+				cur_off_time = 0;
+			}
 		}
 
 	}
@@ -57,10 +86,33 @@ public class PlayerCar : MonoBehaviour {
 	void HandleLapping(){
 		waypoints.Sort ((x, y) => x.distanceFrom(transform.position).CompareTo(y.distanceFrom(transform.position)));
 		distance = WaypointScript.distBetween (waypoints.ToArray () [0], waypoints.ToArray () [1], transform.position);
-		print (distance);
 		if (!primedForLap && distance > 0.5f && distance < 0.55f) {
-			
+			primedForLap = true;
+		} else if (primedForLap && (distance > 0.98f || distance < 0.03f)) {
+			num_laps++;
+			primedForLap = false;
 		}
 	}
 
+ 
+
+	protected virtual void UseUltimate(){}
+
+	protected virtual void UseAbility(){}
+
+	public float getDistance(){
+		return distance;
+	}
+
+	public int getLaps(){
+		return num_laps;
+	}
+
+	public float getAbilCooldown(){
+		return ab_cooldown;
+	}
+
+	public float getUltCooldown(){
+		return ult_cooldown;
+	}
 }
