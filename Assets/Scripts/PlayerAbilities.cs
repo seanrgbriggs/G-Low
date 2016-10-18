75b6105feb77b3007a5438ea7affb49f8beeaf5c
seanrgbriggs <seanrgbriggs@gmail.com>
2016-10-18 15:34:38 -0400

    Abilities are implemented, as a base class.
    UFO now has abilities.

--- Assets/Scripts/PlayerAbilities.cs
+++ Assets/Scripts/PlayerAbilities.cs
@@ -1,11 +1,50 @@
-﻿
-public interface PlayerAbilities{
+﻿using UnityEngine;
 
-    void UseAbility();
+public abstract class PlayerAbilities : MonoBehaviour {
 
-    void UseUltimate();
+    protected float abil_cd;
+    protected float ult_cd;
 
-    void GetAbilityCooldown();
+    protected PlayerCar player;
+    protected int id;
 
-    void GetUltimateCooldown();
+    protected virtual void Start()
+    {
+        abil_cd = 0;
+        ult_cd = 0;
+
+        player = GetComponent<PlayerCar>();
+        id = player.id;
+    }
+
+    protected virtual void Update()
+    {
+        if (Input.GetButtonDown("Ability" + id))
+        {
+            UseAbility();
+        } else if (Input.GetButtonDown("Ultimate" + id))
+        {
+            UseUltimate();
+        }
+    }
+
+    public virtual bool UseAbility()
+    {
+        return (abil_cd >= 1);
+    }
+
+    public virtual bool UseUltimate()
+    {
+        return (ult_cd >= 1);
+    }
+
+    public float GetAbilityCooldown()
+    {
+        return abil_cd;
+    }
+
+    public float GetUltimateCooldown()
+    {
+        return ult_cd;
+    }
 }
