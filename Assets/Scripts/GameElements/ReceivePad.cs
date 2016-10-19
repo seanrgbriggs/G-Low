7c0e0e0e0172d400bcc7f713612c6649c6d57148
Vincent Miller <vmiller0725@gmail.com>
2016-10-18 22:29:24 -0400

    asdf

--- Assets/Scripts/GameElements/ReceivePad.cs
+++ Assets/Scripts/GameElements/ReceivePad.cs
@@ -10,12 +10,18 @@ public class ReceivePad : MonoBehaviour {
     {
         if (col.tag == "Player")
         {
+            print("HI");
             col.GetComponent<Rigidbody>().AddForce(-transform.up * traction, ForceMode.Acceleration);
 
             if (Vector3.Distance(col.transform.up, transform.up) > 0.5f)
             {
-                col.GetComponent<Rigidbody>().AddTorque(Vector3.Cross(col.transform.forward, transform.forward) * alignment_power);
-                col.GetComponent<Rigidbody>().AddTorque(Vector3.Cross(col.transform.up, transform.up) * alignment_power);
+                //col.GetComponent<Rigidbody>().AddTorque(Vector3.Cross(col.transform.forward, transform.forward) * alignment_power);
+                //col.GetComponent<Rigidbody>().AddTorque(Vector3.Cross(col.transform.up, transform.up) * alignment_power);
+
+                Vector3 axis = Vector3.Cross(col.transform.up, transform.up).normalized;
+                float angle = Vector3.Angle(col.transform.up, transform.up);
+
+                col.transform.Rotate(axis, Mathf.Min(angle, Time.deltaTime * alignment_power), Space.World);
             }
         }
     }
