f4c46d2cc99a71daedc889c565762f3ab1a68aa8
Vincent Miller <vmiller0725@gmail.com>
2016-10-19 21:15:13 -0400

    fuck

--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -2,14 +2,24 @@
 using System.Collections;
 
 public class FinishLine : MonoBehaviour {
+    private GameController gc;
 
 	// Use this for initialization
 	void Start () {
-	
+        gc = FindObjectOfType<GameController>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    void OnTriggerEnter(Collider other) {
+        PlayerCar car = other.GetComponent<PlayerCar>();
+        if (car != null) {
+            if (car.getLaps() >= gc.laps_max) {
+                FindObjectOfType<OmniController>().EndGame();
+            }
+        }
+    }
 }
